e7d2fe2e7bd8156283c5799996e59a0823a58210
elwyn-de-neve <elwyn_de_neve@hotmail.com>
2022-07-11 19:23:05 +0200

    summerschool

---
+++ src/assets/data/meaningOfNumbers.js
@@ -0,0 +1,49 @@
+const meaningOfNumbers = [
+    {
+        number: 1,
+        title: 'De Pionier',
+        text: 'begin, oorsprong, eigen kracht, individualiteit, leiderschap'
+    },
+    {
+        number: 2,
+        title: 'De Harmonieschepper',
+        text: 'samen, volgend, begrijpend, dualiteit, vriendelijk'
+    },
+    {
+        number: 3,
+        title: 'De Artiest',
+        text: 'communicatie, expressie, plezier, creatief, onderhandelen'
+    },
+    {
+        number: 4,
+        title: 'De Vormgever',
+        text: 'scheppen, praktisch, discipline, materie, basis (4 elementen)'
+    },
+    {
+        number: 5,
+        title: 'De Reiziger',
+        text: 'flexibel, veranderlijk, vindingrijk, nieuwsgierig, helend'
+    },
+    {
+        number: 6,
+        title: 'De Visionair',
+        text: 'liefde, harmonie, raadgeven, genezing, evenwicht'
+    },
+    {
+        number: 7,
+        title: 'De Levensbeschouwer',
+        text: 'innerlijke rust, stilte, filosofisch, intuïtief, analytisch'
+    },
+    {
+        number: 8,
+        title: 'De Leider',
+        text: 'balans, kracht, macht, erkenning, beloning, ambitie'
+    },
+    {
+        number: 9,
+        title: 'De Leraar',
+        text: 'wijsheid, intuïtie, kennisdeling, helpen, universaliteit, voltooiing'
+    },
+]
+
+export default meaningOfNumbers;
